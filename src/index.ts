3067cf8c81f8c50541d30ecfd514d2a1ede269d6
debika sarkar <debikasarkar713@gmail.com>
2019-10-13 21:16:16 -0400

    recreate project to have map api working

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// const user = new User();
+// const company = new Company();
+
+// console.log(company);
+// console.log(user);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
